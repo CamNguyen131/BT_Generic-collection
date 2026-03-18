90e0ddc2d64e7ef01446c847fa8b5a22371ad599
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 20:56:23 +0700

    Tìm max

---
+++ B3/B3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>() { 3, 7, 2, 9, 5 };
+
+        int max = list[0];
+
+        foreach (int x in list)
+        {
+            if (x > max) max = x;
+        }
+
+        Console.WriteLine("Max = " + max);
+        Console.ReadKey();
+    }
+}
