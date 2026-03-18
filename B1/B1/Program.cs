6754293f421ae5b16de36eb9b0151c1813bcc8f8
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 20:45:08 +0700

    Ko chạy được

--- B1/B1/Program.cs
+++ B1/B1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+
+        list.ForEach(x => Console.WriteLine(x));
+
+        Console.ReadKey();
+    }
+}
