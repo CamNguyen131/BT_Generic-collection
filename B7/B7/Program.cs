77059c5e09f28a46e79ffe75ab6c0460fcc6ad27
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 21:05:40 +0700

    Sắp xếp danh sách

---
+++ B7/B7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>() { 5, 2, 9, 1 };
+
+        for (int i = 0; i < list.Count - 1; i++)
+        {
+            for (int j = i + 1; j < list.Count; j++)
+            {
+                if (list[i] > list[j])
+                {
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+
+        foreach (int x in list)
+        {
+            Console.WriteLine(x);
+        }
+
+        Console.ReadKey();
+    }
+}
