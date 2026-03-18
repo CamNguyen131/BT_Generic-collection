9fe977d1087e272f0d9ecfb83afdf8f4f9b4ff08
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 20:58:53 +0700

    update

--- B4/B4/Program.cs
+++ B4/B4/Program.cs
@@ -1,20 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        List<int> list = new List<int>() { 1, 2, 3, 4, 6 };
+        List<int> list = new List<int>() { 1, 2, 4, 5, 6 };
 
-        int count = 0;
-
-        foreach (int x in list)
-        {
-            if (x % 2 == 0) count++;
-        }
+        int count = list.Count(x => x % 2 == 0);
 
         Console.WriteLine("So chan = " + count);
+
         Console.ReadKey();
     }
 }
