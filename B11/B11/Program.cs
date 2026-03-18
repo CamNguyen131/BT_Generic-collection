24a8647d392f5f7fa6a44be20a0ed84324f16578
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 21:07:22 +0700

    Lưu danh sách sinh viên

---
+++ B11/B11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Student
+{
+    public int Id;
+    public string Name;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Student> list = new List<Student>()
+        {
+            new Student{Id=1, Name="An"},
+            new Student{Id=2, Name="Binh"}
+        };
+
+        foreach (var s in list)
+        {
+            Console.WriteLine(s.Id + " - " + s.Name);
+        }
+
+        Console.ReadKey();
+    }
+}
