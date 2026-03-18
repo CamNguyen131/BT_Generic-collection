569e0f443bec64a81e3d0cc780bd13befba1f4e1
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 21:06:58 +0700

    Tìm phần tử nhỏ nhất

---
+++ B10/B10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>() { 5, 2, 8, 1 };
+
+        int min = list[0];
+
+        foreach (int x in list)
+        {
+            if (x < min) min = x;
+        }
+
+        Console.WriteLine("Min = " + min);
+        Console.ReadKey();
+    }
+}
