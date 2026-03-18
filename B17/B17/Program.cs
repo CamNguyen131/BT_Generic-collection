98fc97e542b28c13b021dea1b9c02593cbb26bdb
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 21:09:36 +0700

    Tìm phần tử xuất hiện nhiều nhất

---
+++ B17/B17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap chuoi: ");
+        string str = Console.ReadLine();
+
+        Dictionary<char, int> count = new Dictionary<char, int>();
+
+        foreach (char c in str)
+        {
+            if (count.ContainsKey(c))
+                count[c]++;
+            else
+                count[c] = 1;
+        }
+
+        foreach (var item in count)
+        {
+            Console.WriteLine(item.Key + " : " + item.Value);
+        }
+
+        Console.ReadKey();
+    }
+}
