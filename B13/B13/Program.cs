e81fef887e4bf089493a14dcfeff34e4c9a2e6a9
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 21:08:08 +0700

    Đếm số sinh viên

---
+++ B13/B13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Student
+{
+    public int Id;
+    public string Name;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Student> list = new List<Student>()
+        {
+            new Student{Id=1, Name="An"},
+            new Student{Id=2, Name="Binh"}
+        };
+
+        Console.Write("Nhap ten: ");
+        string key = Console.ReadLine();
+
+        foreach (var s in list)
+        {
+            if (s.Name.ToLower().Contains(key.ToLower()))
+            {
+                Console.WriteLine(s.Name);
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
