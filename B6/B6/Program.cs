c3893ca97bca3cf1b4f4a76d222ed22adb48149e
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 21:05:06 +0700

    Kiểm tra tồn tại

---
+++ B6/B6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>() { "An", "Binh", "Cuong" };
+
+        Console.Write("Nhap ten: ");
+        string name = Console.ReadLine();
+
+        bool found = false;
+
+        foreach (string s in list)
+        {
+            if (s == name) found = true;
+        }
+
+        if (found) Console.WriteLine("Ton tai");
+        else Console.WriteLine("Khong ton tai");
+
+        Console.ReadKey();
+    }
+}
