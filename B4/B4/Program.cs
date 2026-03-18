44fb4059a2c7527b8adb0f562420fc9e97ff2348
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 20:58:14 +0700

    Số chẵn

---
+++ B4/B4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>() { 1, 2, 3, 4, 6 };
+
+        int count = 0;
+
+        foreach (int x in list)
+        {
+            if (x % 2 == 0) count++;
+        }
+
+        Console.WriteLine("So chan = " + count);
+        Console.ReadKey();
+    }
+}
