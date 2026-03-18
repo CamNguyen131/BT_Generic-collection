7b9cdebaea3a5422a37e893a395466ccff86cfe3
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 20:53:57 +0700

    Tính tổng

---
+++ B2/B2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        List<int> list = new List<int>();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Nhap so: ");
+            int x = int.Parse(Console.ReadLine());
+            list.Add(x);
+            sum += x;
+        }
+
+        Console.WriteLine("Tong = " + sum);
+        Console.ReadKey();
+    }
+}
