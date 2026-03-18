0daf8f62461591ecf29baf446cf252c7c79e1fa8
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 20:59:49 +0700

    Xóa phần tử

---
+++ B5/B5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>() { "An", "Binh", "Cuong" };
+
+        Console.Write("Nhap ten can xoa: ");
+        string name = Console.ReadLine();
+
+        list.Remove(name);
+
+        foreach (string s in list)
+        {
+            Console.WriteLine(s);
+        }
+
+        Console.ReadKey();
+    }
+}
