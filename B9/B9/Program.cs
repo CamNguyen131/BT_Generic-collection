dd4751b9003b6035e1783b905c33dd900f4cba65
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 21:06:26 +0700

    Trùng lặp

---
+++ B9/B9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>() { 1, 2, 3, 4 };
+
+        list.Reverse();
+
+        foreach (int x in list)
+        {
+            Console.WriteLine(x);
+        }
+
+        Console.ReadKey();
+    }
+}
