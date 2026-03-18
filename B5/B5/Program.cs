f2649b28ed599f9f7f7bab63e2a37ec25859aedf
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 21:00:25 +0700

    loi hiển thị

--- B5/B5/Program.cs
+++ B5/B5/Program.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         List<string> list = new List<string>() { "An", "Binh", "Cuong" };
 
-        Console.Write("Nhap ten can xoa: ");
+        Console.Write("Nhap ten: ");
         string name = Console.ReadLine();
 
         list.Remove(name);
 
-        foreach (string s in list)
-        {
-            Console.WriteLine(s);
-        }
+        list.ForEach(x => Console.WriteLine(x));
 
         Console.ReadKey();
     }
