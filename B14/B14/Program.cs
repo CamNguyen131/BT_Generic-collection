2e73e3f5e80e236ca394467405fb82db223351cc
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-18 21:08:34 +0700

    Xóa sinh viên theo ID

---
+++ B14/B14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Student
+{
+    public int Id;
+    public string Name;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Student> list = new List<Student>()
+        {
+            new Student{Id=1, Name="An"},
+            new Student{Id=2, Name="Binh"}
+        };
+
+        Console.Write("Nhap ID: ");
+        int id = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i].Id == id)
+            {
+                list.RemoveAt(i);
+                break;
+            }
+        }
+
+        foreach (var s in list)
+        {
+            Console.WriteLine(s.Id + " - " + s.Name);
+        }
+
+        Console.ReadKey();
+    }
+}
